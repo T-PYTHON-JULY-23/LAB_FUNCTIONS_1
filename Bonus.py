70e222d3713a34dc8faf2000af1b54dc04c24fe3
= <=>
2023-07-27 14:14:01 +0300

    bonus complete

--- Bonus.py
+++ Bonus.py
@@ -1,28 +1,10 @@
-def numbers(x:int):
+def numbers(x:int)->str:
     ''' pyrmid '''
+    
     for i in range(x,0,-1): #x1,x2,x3,x4,x5 
       for j in range (i, 0,-1): #x1,x2,x3,x4
         print(j,end=" ")
       print()
-    
-numbers(5)   
-print(numbers.__doc__)
+    return numbers   
+print(numbers)
 
-
-
-
-
-
-
-
-
-   
-
-
-
- 
-
-
-
-
- 
