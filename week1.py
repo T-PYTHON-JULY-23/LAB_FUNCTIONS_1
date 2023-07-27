11bcd8172494e07b3297911352b36cc2a1cd0e9f
Banan Alburaidi <bananburaidi@gmail.com>
2023-07-27 13:10:49 +0300

    adding new file

---
+++ week1.py
@@ -0,0 +1,18 @@
+def ArrayNumbers(number):
+    ''' gdgdd '''
+
+    ieratation=number
+    #for number in range(number,1):
+    while ieratation >=1:
+
+     while number>=1:
+            print (number,end=" ") # to delete the new line and make all the output itteration in one line
+            number=number-1
+
+    ieratation=ieratation-1    
+
+
+
+
+inputNumber=int(input("Enter a number to show to array:"))
+ArrayNumbers(inputNumber)
