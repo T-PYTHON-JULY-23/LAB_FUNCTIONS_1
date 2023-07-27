af33eed4e843775789f0885e320d9df8f86e782e
Lenah11 <lenhh11@gmail.com>
2023-07-27 13:28:40 +0300

    bonus

--- Bonus.py
+++ Bonus.py
@@ -2,12 +2,13 @@
 
 def numbers (num:int):
     '''This function print the result as hierarchy only'''
+    output =" "
     for i in range(num,0, -1):
         for j in range(i,0, -1):
             print(j,end=" ")
-        print()
-        
+        print(output)
+    return(output)        
 
-numbers(5)
+print(numbers(5))
 
 print(numbers.__doc__)
