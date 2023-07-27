d7cb34324e372a43115211014e1af33c55285f8c
ghaida <ghaida@gmail.com>
2023-07-27 11:46:39 +0300

    lab is solved

---
+++ function.py
@@ -0,0 +1,13 @@
+def pyramid (x:int)->int:
+    '''prints out the result formatted like the pyramid'''
+
+    for row in range(x, 0, -1):
+      for col in range(row, 0,-1):
+        print(col, end=" ")
+        col=col-1
+    
+      print("\n")
+        
+        
+        
+pyramid(5)
