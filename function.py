4a0a92eb82691d38905c4dfdbaa22ed5fc18fb16
Nada <Nada@gmail.com>
2023-07-27 12:21:30 +0300

    Function Lab

---
+++ function.py
@@ -0,0 +1,27 @@
+def numbers(x:int):
+    ''' pyrmid '''
+    for i in range(x,0,-1): #x1,x2,x3,x4,x5 
+      for j in range (i, 0,-1): #x1,x2,x3,x4
+        print(j,end=" ")
+      print()
+    
+numbers(5)   
+print(numbers.__doc__)
+
+
+
+
+
+
+
+
+   
+
+
+
+ 
+
+
+
+
+ 
