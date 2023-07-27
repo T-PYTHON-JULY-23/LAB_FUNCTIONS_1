e927dfa62b63913f78b2f3e4e352bdcefa531651
ghaida <ghaida@gmail.com>
2023-07-27 13:08:25 +0300

    bouns solved

---
+++ bouns.py
@@ -0,0 +1,16 @@
+def pyramid (x):
+    
+    '''prints out the result formatted like the pyramid'''
+    string=""
+
+    for row in range(x, 0, -1):
+      for col in range(row, 0,-1):
+        string=string+str(col)+" "
+     
+      string=string+"\n"
+    
+    return string
+
+
+print(pyramid(6))
+        
