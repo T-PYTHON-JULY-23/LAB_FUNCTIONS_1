5132622075fcd130ef200bf8bed4ba5cfefa87bd
xluuu <mlikmozini@gmail.com>
2023-07-30 17:24:17 +0300

    Lab solved

--- Func.py
+++ Func.py
@@ -1,21 +1,8 @@
-# # LAB_FUNCTIONS_1
-
-
-# ## Create a function that takes 1 parameter of type int , then it prints out the result formatted like the following patter (if we give it 5 for example):
-
-
 num = 5
-def patter(num):
+def patter(num):#this function will take a number and return it as reverse order Step by Step
   for n in range(num, 0,-1):
     for j in range(n, 0,-1):
         print(j, end=" ")
-    print('\r')
+    print('\r')#New line same as (\n) but r is smoother 
     
 patter(num)
-# 5 4 3 2 1   
-# 4 3 2 1   
-# 3 2 1   
-# 2 1   
-# 1
-
-# ### Document the newly created function. describe what it does, then print the documentation. 
