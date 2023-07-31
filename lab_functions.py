6df241f8c12aaa48cc36b2cb202f4c6497912119
SarahAlhamad <140398026+SarahAlhamad@users.noreply.github.com>
2023-07-31 20:34:55 +0300

    Update lab_functions.py

--- lab_functions.py
+++ lab_functions.py
@@ -1,9 +1,9 @@
 def number(num:int ) -> int:
-   
+   '''this is Numbers sppear to me hierarchical '''
    for line in range(num,0,-1):
     for  i in range(line,0,-1):
         print( i , end="" )
     print()
 
-   
+print(number.__doc__)
 number(5)
