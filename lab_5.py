b6f9a1390bcd8cccde58b3e435b151ee055b5f3b
Hassan Albukhaytan <hassan.s.albukhaytan@gmail.com>
2023-08-01 12:08:13 +0300

    Hassan

--- lab_5.py
+++ lab_5.py
@@ -1,8 +1,10 @@
 def Reverse_Order(y):
+    """This functions returns an invers pyramid of the numbers"""
     for i in range(y, 0, -1):
         for j in range(i, 0, -1):
-            print(j, end=' ')
+            print(j, end=" ")
         print()
 
 
+print(Reverse_Order.__doc__)
 Reverse_Order(int(input("enter the parameter : ")))
