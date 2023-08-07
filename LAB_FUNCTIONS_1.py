d917be672b19dd2f76372f226f46ec3db0cbe3c1
nada <nada_abdullah_8@hotmail.com>
2023-08-07 14:21:20 +0300

    another solution

--- LAB_FUNCTIONS_1.py
+++ LAB_FUNCTIONS_1.py
@@ -4,7 +4,7 @@ def InvertedNum (number:int):
     
 
     for num in reversed(range(1,number+1)) :
-        for num2 in reversed(range(num,number+1)):
+        for num2 in reversed(range(1,num+1)):
              print(num2 , end=" ")
 
         print("\n")
